91e57e8826784247ae641f2a008c5a652f153ebb
Braeden Kloke <braedenkloke@gmail.com>
2024-03-07 13:38:01 -0500

    feat(login): add login use case
    Add LoginController with stubbed method authenticate. CLI prompts user for username and password and logs them into the system.

--- src/main/ClubManagementCLI.java
+++ src/main/ClubManagementCLI.java
@@ -1,12 +1,43 @@
 package main;
 
+import main.controllers.LoginController;
+
+import java.util.Scanner;
+
 /**
  * Class representing the command line interface (CLI) for the
  * Health and Fitness Club Management System.
+ *
+ * @author Braeden Kloke
+ * @version March 7, 2024
+ * Implement login use case.
  */
 public class ClubManagementCLI {
 
     public ClubManagementCLI() {
-        System.out.println("Hello world!");
+        login();
+    }
+
+    /**
+     * Prompts user for username and password. Logs user into system
+     * if login attempt is valid.
+     *
+     * @return True if valid login attempt, false otherwise.
+     *
+     * @author Braeden Kloke
+     */
+    private boolean login() {
+        String username, password;
+        Scanner scanner = new Scanner(System.in);
+        LoginController loginController = new LoginController();
+
+        // prompt user for username and password
+        System.out.println("Enter username: ");
+        username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        password = scanner.nextLine();
+
+        // authenticate user
+        return loginController.authenticate(username, password);
     }
 }
--- src/main/controllers/LoginController.java
+++ src/main/controllers/LoginController.java
@@ -2,6 +2,25 @@ package main.controllers;
 
 /**
  * Class representing a controller to handle login requests.
+ *
+ * @author Braeden Kloke
+ * @version March 7, 2024
+ * Initialize interface and stub implementations.
  */
 public class LoginController {
+
+    public LoginController() {
+
+    }
+
+    /**
+     * Authenticates a user of the system.
+     *
+     * @param username Name of user to authenticate.
+     * @param password Password to authenticate user.
+     * @return True if user authenticated, false otherwise.
+     */
+    public boolean authenticate(String username, String password) {
+        return true;
+    }
 }
