41d205accafa4f209a8c01b7f788005bcc67e5df
Arthur Atangana <arthur.atangana@protonmail.com>
2024-03-19 19:28:03 -0400

    Add ddl.sql in accordance to the schema
    created all tables, and two new data types
    
    - d_o_w as in day of the week enum for the scheduling
    - stat_type to distinguish between current stats and goal stats

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,16 +1,97 @@
-CREATE DATABASE club
+CREATE DATABASE club;
 
-CREATE TABLE users (
-	user_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(255) NOT NULL,
-	last_name VARCHAR(255) NOT NULL,
-	email TEXT NOT NULL UNIQUE,
-	password text NOT NULL
-)
+CREATE TABLE users
+(
+    user_id    SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    email      TEXT         NOT NULL UNIQUE,
+    password   text         NOT NULL
+);
 
-CREATE TABLE admins (
-	admin_id SERIAL PRIMARY KEY,
-	user_id INT REFERENCES users(user_id),
-	salary INT
-)
+CREATE TABLE admins
+(
+    admin_id SERIAL PRIMARY KEY,
+    user_id  INT REFERENCES users (user_id),
+    salary   INT
+);
 
+CREATE TABLE trainers
+(
+    trainer_id SERIAL PRIMARY KEY,
+    user_id    INT REFERENCES users (user_id),
+    salary     INT
+);
+
+CREATE TABLE certificates
+(
+    certificate_id SERIAL PRIMARY KEY,
+    trainer_id     INT REFERENCES trainers (trainer_id),
+    name           VARCHAR(255)
+);
+
+
+CREATE TABLE members
+(
+    member_id SERIAL PRIMARY KEY,
+    user_id   INT REFERENCES users (user_id)
+);
+
+CREATE TABLE classes
+(
+    class_id   SERIAL PRIMARY KEY,
+    class_name VARCHAR(255)
+);
+
+CREATE TABLE room
+(
+    room_id  SERIAL PRIMARY KEY,
+    capacity INT NOT NULL
+);
+
+CREATE TABLE participants
+(
+    member_id INT REFERENCES members (member_id),
+    class_id  INT REFERENCES classes (class_id),
+    PRIMARY KEY (member_id, class_id)
+);
+
+CREATE TYPE d_o_w AS ENUM ('monday','tuesday','wednesday','thursday','friday','saturday','sunday');
+
+CREATE TABLE timeslots
+(
+    timeslot_id SERIAL PRIMARY KEY,
+    class_id    INT REFERENCES classes (class_id),
+    trainer_id  INT REFERENCES trainers (trainer_id),
+    room_id     INT REFERENCES room (room_id),
+    day_of_week D_O_W NOT NULL,
+    time        TIME
+);
+
+CREATE TYPE stat_type AS ENUM ('goal','current');
+
+CREATE TABLE fitness_stats
+(
+    member_id       INT REFERENCES members (member_id),
+    goal_or_current STAT_TYPE,
+    weight          REAL NOT NULL,
+    hours           INT  NOT NULL,
+    flexibility     INT  NOT NULL,
+    PRIMARY KEY (member_id, goal_or_current)
+);
+
+CREATE TABLE payments
+(
+    payment_id SERIAL PRIMARY KEY,
+    member_id  INT REFERENCES members (member_id),
+    class_id   INT REFERENCES classes (class_id),
+    amount     REAL NOT NULL
+);
+
+CREATE TABLE equipment
+(
+    equipment_id             SERIAL PRIMARY KEY,
+    name                     VARCHAR(255) NOT NULL,
+    recurrence               INT          NOT NULL,
+    date_of_last_maintenance DATE
+);
