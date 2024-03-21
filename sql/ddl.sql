3147d60c89e413986102ff097129d72487209b21
Arthur Atangana <arthur.atangana@protonmail.com>
2024-03-20 22:09:31 -0400

    Add time slots enum for the scheduling
    Also fix minor semantic things

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -40,10 +40,10 @@ CREATE TABLE members
 CREATE TABLE classes
 (
     class_id   SERIAL PRIMARY KEY,
-    class_name VARCHAR(255)
+    name VARCHAR(255)
 );
 
-CREATE TABLE room
+CREATE TABLE rooms
 (
     room_id  SERIAL PRIMARY KEY,
     capacity INT NOT NULL
@@ -57,15 +57,15 @@ CREATE TABLE participants
 );
 
 CREATE TYPE d_o_w AS ENUM ('monday','tuesday','wednesday','thursday','friday','saturday','sunday');
-
+CREATE TYPE time_slot as ENUM ('09:00:00','10:00:00','11:00:00','12:00:00','13:00:00','14:00:00','15:00:00','16:00:00');
 CREATE TABLE timeslots
 (
     timeslot_id SERIAL PRIMARY KEY,
     class_id    INT REFERENCES classes (class_id),
     trainer_id  INT REFERENCES trainers (trainer_id),
     room_id     INT REFERENCES room (room_id),
-    day_of_week D_O_W NOT NULL,
-    time        TIME
+    day_of_week D_O_W     NOT NULL,
+    time        TIME_SLOT NOT NULL
 );
 
 CREATE TYPE stat_type AS ENUM ('goal','current');
